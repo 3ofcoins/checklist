b4849e971401477048722a8861f2f123ea8f4058
Maciej Pasternacki <maciej@pasternacki.net>
2012-06-10 21:41:43 +0200

    Step spec cleanup

--- spec/checklist/step_spec.rb
+++ spec/checklist/step_spec.rb
@@ -15,43 +15,41 @@ describe Checklist::Step do
 end
 
 describe Checklist::Step, "#run!" do
-  before(:each) do
-    @called = false
-    @raise = false
-    @code = Proc.new do
-      if @raise
-        raise RuntimeError, "as requested"
-      else
-        @called = true
-      end
-    end
-    @step = Checklist::Step.new(
+  let(:code_body) do
+    code_body = double('code body')
+    code_body.should_receive(:poke!)
+    code_body
+  end
+  let(:step) do
+    Checklist::Step.new(
       'the challenge',
       'the response',
       'the description',
-      @code)
-    STDOUT.stub(:puts)
+      lambda { code_body.poke! } )
   end
 
+  before(:each) { STDOUT.stub(:puts) }
+
   it "runs the proc from code field" do
-    @step.run!
-    @called.should be_true
+    step.run!
   end
 
   it "prints challenge at the beginning and challenge&response at the end" do
-    STDOUT.should_receive(:puts).with("** the challenge ...").once.ordered
-    STDOUT.should_receive(:puts).with("** the challenge the response").once.ordered
-    @step.run!
+    STDOUT.should_receive(:puts).once.with("** the challenge ...").ordered
+    STDOUT.should_receive(:puts).once.with("** the challenge the response").ordered
+    STDOUT.should_receive(:puts).exactly(0).times
+    step.run!
   end
 
   it "does not catch exceptions raised by code" do
-    @raise = true
+    step.code = lambda { code_body.poke! ; raise RuntimeError }
     STDOUT.should_receive(:puts).with("** the challenge ...").once
-    lambda { @step.run! }.should raise_error(RuntimeError, "as requested")
+    STDOUT.should_receive(:puts).exactly(0).times
+    lambda {  step.run! }.should raise_error(RuntimeError)
   end
 end
 
-describe Checklist, '.step' do
+describe Checklist, '::step' do
   it "Creates a runnable Checklist::Step instance" do
     called = false
     step = Checklist.step('the challenge', 'the response', 'a description') do
